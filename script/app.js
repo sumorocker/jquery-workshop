d1c2160fdf5855dc6688a91a1332f27c01160227
sumorocker <macdonald.allan@live.com>
2015-12-10 20:09:56 +0000

    Exercise-1 added

--- script/app.js
+++ script/app.js
@@ -1,1 +1,17 @@
-// Write your code here!
+$(document).ready(function(){
+$(".announcement").prepend("<button>x</button>");    
+
+
+$("button").on('click', function(){
+    // $(".announcement p").toggle('slow');
+   if ($(this).text() === 'x'){
+   $(this).text("Show Announcement");
+   $(this).siblings().hide("slow");
+   }else{
+   $(this).text("x");
+   $(this).siblings().show("slow");
+   }
+});
+
+
+});
