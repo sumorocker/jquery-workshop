ff554499d3952fb8f51b629441b051974df4dbf6
sumorocker <macdonald.allan@live.com>
2015-12-10 22:32:54 +0000

    exercise-4

--- script/app.js
+++ script/app.js
@@ -1,12 +1,19 @@
-$(document).ready(function(){
-$('img').on('click', function(){
-    $('body').append('<div class="overlay"><img src='+
-                        $(this).attr('src')+'><p>'+
-                        $(this).attr('alt')+ '</p></div>');
-    $('.overlay').on('click', function(){
-    $(this).remove();
-});
-    
-});
+$(document).ready(function() {
+    $('#signup-form').on('submit', function(event) {
+        alert($('input[name="password"]').val())
+        if ($('input[name="password"]').val().length < 10) {
+            event.preventDefault();
+            alert("Your password is not long enough");
+            return;
+        }
+        if ($('input[name="password"]').val() != $('input[name="password2"]').val()) {
+            event.preventDefault();
+            alert("Your passwords didn't match");
+        }
+        if ($('input[name="user"]').val().length < 5) {
+            event.preventDefault();
+            alert("Your username is too short");
+        }
+    });
 
 });
