5b29dfa2647d6c60524c1e2275c3e714d1841138
sumorocker <macdonald.allan@live.com>
2015-12-11 15:55:40 +0000

    exercise-4

--- script/app.js
+++ script/app.js
@@ -1,19 +1,7 @@
 $(document).ready(function() {
-    $('#signup-form').on('submit', function(event) {
-        alert($('input[name="password"]').val())
-        if ($('input[name="password"]').val().length < 10) {
-            event.preventDefault();
-            alert("Your password is not long enough");
-            return;
-        }
-        if ($('input[name="password"]').val() != $('input[name="password2"]').val()) {
-            event.preventDefault();
-            alert("Your passwords didn't match");
-        }
-        if ($('input[name="user"]').val().length < 5) {
-            event.preventDefault();
-            alert("Your username is too short");
-        }
-    });
+jQuery.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=montreal').then(
+    function(res) {
+        $("article h2").after(res)
+    })
 
 });
